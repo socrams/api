17f2d7ff9c9ad4218f3c24924feb725df1063dd2
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2022-09-28 16:11:01 -0300

    9

--- index.php
+++ index.php
@@ -39,11 +39,7 @@ $app->add(function (Request $request, RequestHandlerInterface $handler): Respons
         $response->getBody()->write('"Hello world! Regrese"');
         return $response;
     }); 
-    $app->post('/', function (Request $request, Response $response, $args) {
-        $response->getBody()->write('"Hello world! Regrese"');
-        return $response;
-    }); 
-
+    
 $app->post('/identificador/', usuarioControlador::class . ':mostrarUsuario' );
 
 $app->run();
