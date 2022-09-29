ee329641e570e101d4e848c21a9b38bbd9a3af48
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2022-09-29 11:11:25 -0300

    a1

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $app->add(function (Request $request, RequestHandlerInterface $handler): Respons
         return $response;
     }); 
 
-    // $app->post('/', function (Request $request, Response $response, $args) {
+    // $app->get('/', function (Request $request, Response $response, $args) {
     //     $response->getBody()->write('"Hello world!"');
     //     return $response;
     // }); 
